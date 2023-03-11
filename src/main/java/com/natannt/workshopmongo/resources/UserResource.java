58d1bf9a63c0c71a473e5aaef8972721868d111a
Natan Dadalt <natan.db@gmail.com>
2023-03-10 21:31:24 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/natannt/workshopmongo/resources/UserResource.java
+++ src/main/java/com/natannt/workshopmongo/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.natannt.workshopmongo.domain.Post;
 import com.natannt.workshopmongo.domain.User;
 import com.natannt.workshopmongo.dto.UserDTO;
 import com.natannt.workshopmongo.services.UserService;
@@ -62,6 +63,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 	
