2108c52f8ac49b1746a063584a49513d5bf3dd16
Natan Dadalt <natan.db@gmail.com>
2023-03-10 21:36:46 -0300

    Obtendo post por id

---
+++ src/main/java/com/natannt/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.natannt.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.natannt.workshopmongo.domain.Post;
+import com.natannt.workshopmongo.repository.PostRepository;
+import com.natannt.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+		
+	}
+	
+}
