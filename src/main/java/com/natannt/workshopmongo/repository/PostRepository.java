53a1d919661daa6a4c5d0f43bf3ab4c7bde4cf18
Natan Dadalt <natan.db@gmail.com>
2023-03-11 21:56:08 -0300

    Consulta simples com query methods

--- src/main/java/com/natannt/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/natannt/workshopmongo/repository/PostRepository.java
@@ -1,9 +1,12 @@
 package com.natannt.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.natannt.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post,String>{
 	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
