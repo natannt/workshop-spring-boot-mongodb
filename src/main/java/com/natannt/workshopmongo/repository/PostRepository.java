0e61633aa8581184793fe320d6e595d9799a6c87
Natan Dadalt <natan.db@gmail.com>
2023-03-11 22:05:20 -0300

    Consulta simples com @Query

--- src/main/java/com/natannt/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/natannt/workshopmongo/repository/PostRepository.java
@@ -3,10 +3,14 @@ package com.natannt.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 
 import com.natannt.workshopmongo.domain.Post;
 
 public interface PostRepository extends MongoRepository<Post,String>{
+	
+	@Query("{ 'title': {$regex: ?0, $options: 'i'}}")
+	List<Post> searchTitle(String Text);
 	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
