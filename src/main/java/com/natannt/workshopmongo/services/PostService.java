92cfe3a0ea918d7c8de155dbf01aa334528cf7f6
Natan Dadalt <natan.db@gmail.com>
2023-03-11 22:54:30 -0300

    Consulta com varios criterios

--- src/main/java/com/natannt/workshopmongo/services/PostService.java
+++ src/main/java/com/natannt/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.natannt.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,4 +28,9 @@ public class PostService {
 		return repo.searchTitle(text);
 	}
 	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+		
+	}
 }
